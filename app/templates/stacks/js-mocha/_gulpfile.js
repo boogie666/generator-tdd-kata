59b801e04f1cde3c7596aee62937d7daeed94ae1
John Lianoglou <john.lianoglou@viacom.com>
2015-08-05 19:30:55 -0400

    Adjusting indentation of gulpfile

--- app/templates/stacks/js-mocha/_gulpfile.js
+++ app/templates/stacks/js-mocha/_gulpfile.js
@@ -1,6 +1,8 @@
-var gulp = require('gulp'),
-    mocha = require('gulp-mocha'),
-    chai = require('chai');
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var chai = require('chai');
 
 chai.config.showDiff = false;
 chai.should();
@@ -8,12 +10,15 @@ chai.should();
 Error.stackTraceLimit = 2;
 
 gulp.task('test', function() {
-  gulp.src('src/**/*Test.js', {
-    read: false
-  }).pipe(mocha()).on('error', function(e){
-    console.warn(e.message);
-    this.emit('end');
-  });
+  gulp
+    .src('src/**/*Test.js', {
+      read: false
+    })
+    .pipe(mocha())
+    .on('error', function(e) {
+      console.warn(e.message);
+      this.emit('end');
+    });
 });
 
 gulp.task('watch', function(){
