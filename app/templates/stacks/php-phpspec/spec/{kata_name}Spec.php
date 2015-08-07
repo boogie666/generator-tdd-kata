750225d38c1e7631b0275ed381530e2a63038a45
John Lianoglou <john.lianoglou@viacom.com>
2015-08-06 21:58:10 -0400

    Added a starting fail :)

--- app/templates/stacks/php-phpspec/spec/{kata_name}Spec.php
+++ app/templates/stacks/php-phpspec/spec/{kata_name}Spec.php
@@ -8,10 +8,10 @@ use Prophecy\Argument;
 class <%= kata.pascalized %>Spec extends ObjectBehavior
 {
     /**
-     * This sample test proves everything works.  Delete it and add your own.
+     * This little test gives you your first "red" :-)
      */
-    function it_is_initializable()
+    function it_is_your_first_fail()
     {
-        $this->shouldHaveType('TDDKatas\StringCalculator');
+         $this->shouldHaveType('TDDKatas\InvalidClassname');
     }
 }
